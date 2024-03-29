95a09e17597913c7b4597a6bb84d4fc0c9a41de9
benson1231 <144930897+benson1231@users.noreply.github.com>
2024-03-30 04:13:46 +0800

    Create kegg_run.R

---
+++ R/kegg_run.R
@@ -0,0 +1,49 @@
+kegg_run <- function(data_path=data_path,
+                     file,
+                     all_gene=FALSE,
+                     list,
+                     list_id="ensembl"   # ensembl/symbol
+){
+  # select gene in list
+  if(all_gene==TRUE){
+    df <- readxl::read_xlsx(file.path(data_path, file))
+    cat(c(" >- input all gene and run KEGG","\n"))
+  } else {
+    if(list_id=="ensembl"){
+      df <- readxl::read_xlsx(file.path(data_path, file)) %>% 
+        filter(ENSEMBL %in% list)
+    } else if(list_id=="symbol"){
+      df <- readxl::read_xlsx(file.path(data_path, file)) %>% 
+        filter(SYMBOL %in% list)
+    } else{
+      cat(c(" <- error: check list_id","\n"))
+      return(NULL)
+    }
+    cat(c(" >- input selected gene and run ORA"))
+  } 
+  
+  # change KEGG Organism Code from https://www.genome.jp/kegg/catalog/org_list.html 
+  kegg_organism <-  "hsa"  # human is "hsa"
+  cat(c(" -> organism type is", kegg_organism,"\n"))
+  
+  # selcet log2FC value
+  kegg_gene_list <- df$M
+  # Name vector with ENTREZ ids
+  names(kegg_gene_list) <- df$ENTREZID
+  # omit any NA values and sort the list in decreasing order
+  kegg_gene_list<- na.omit(kegg_gene_list) %>% 
+    sort(., decreasing = TRUE)
+  cat(c(" -> length of the gene list is",length(kegg_gene_list),"\n"))
+  # Run KEGG
+  cat(c(" -> running KEGG", "\n"))
+  kegg <- gseKEGG(geneList     = kegg_gene_list,
+                  organism     = kegg_organism,
+                  nPerm        = 10000,
+                  minGSSize    = 3,
+                  maxGSSize    = 800,
+                  pvalueCutoff = 0.05,
+                  pAdjustMethod = "none",
+                  keyType       = "ncbi-geneid")
+  return(kegg)
+}
+
