4c108ccdeedc65a54ad94c061229190b9dd80bce
benson1231 <144930897+benson1231@users.noreply.github.com>
2024-03-30 04:12:28 +0800

    Update RNAseq_function.R

--- RNAseq_function.R
+++ RNAseq_function.R
@@ -3,7 +3,8 @@
 draw_heatmap <- function(data_path=data_path,
                          file=file,
                          log_crit=3,
-                         groups=groups
+                         groups=groups,
+                         show_row_names = TRUE
 ){
   data <- readxl::read_xlsx(file.path(data_path, file)) # 讀檔
   data <- data %>% filter(abs(M)>log_crit)  # filter log2FC criteria
@@ -33,7 +34,7 @@ draw_heatmap <- function(data_path=data_path,
                                ip_Y_V_S_CO_BAP,ip_Y_V_S_LCD_BAP,ip_Y_V_S_HCD_BAP)
   } else{
     cat(c("<- please type in groups", "\n"))
-    break
+    return(NULL)
   }
   
   cat(c("<- filtering data", "\n"))
@@ -66,7 +67,8 @@ draw_heatmap <- function(data_path=data_path,
   cat(c("<- drawing heatmap", "\n"))
   
   ComplexHeatmap::Heatmap(mat_scale, top_annotation = ha, cluster_columns = F, 
-                          show_row_names = T, show_column_names = F,
+                          show_row_names = show_row_names, 
+                          show_column_names = F,
                           name = "Z-score"
   )
   
@@ -78,7 +80,8 @@ draw_from_list <- function(list,
                            id="ENSEMBL",
                            cluster=TRUE,
                            anno=TRUE,
-                           title=""# ENSEMBL/SYMBOL
+                           title="",
+                           show_row_names = TRUE# ENSEMBL/SYMBOL
 ){
   if(id=="ENSEMBL"){
     list <- list  
@@ -101,7 +104,7 @@ draw_from_list <- function(list,
       filter(.,rownames(new_df) %in% list)
   }else{
     cat(c("<- error, check gene id", "\n"))
-    break
+    return(NULL)
   }
   
   if(groups == "AS"){
@@ -121,7 +124,7 @@ draw_from_list <- function(list,
                                ip_Y_V_S_CO_BAP,ip_Y_V_S_LCD_BAP,ip_Y_V_S_HCD_BAP)
   } else{
     cat(c("<- please type in groups", "\n"))
-    break
+    reture(NULL)
   }
   cat(c("<- filtering data", "\n"))
   
@@ -154,12 +157,15 @@ draw_from_list <- function(list,
   
   if(anno==T){
     ComplexHeatmap::Heatmap(mat_scale, top_annotation = ha, cluster_columns = F, 
-                            show_row_names = T, show_column_names = F,cluster_rows = cluster,
+                            show_row_names = show_row_names,
+                            show_column_names = F,cluster_rows = cluster,
                             name = "Z-score", row_title = title
     )
   }else{
     ComplexHeatmap::Heatmap(mat_scale, cluster_columns = F, 
-                            show_row_names = T, show_column_names = F,cluster_rows = cluster,
+                            show_row_names = show_row_names, 
+                            show_column_names = F,
+                            cluster_rows = cluster,
                             name = "Z-score", row_title = title
     )
   }
@@ -180,7 +186,111 @@ get_deg <- function(data_path=data_path,
     data <- data %>% filter(M < log_crit[2])
   }else{
     cat(c("<- error, check direction", "\n"))
+    return(NULL)
   }
   list <- data$ENSEMBL 
   return(list)# 抓出差異ensembl id
 }
+
+# gsea_run ----------------------------------------------------------------
+gsea_run <- function(data_path=data_path,
+                     file,
+                     all_gene=FALSE, 
+                     list,
+                     list_id="ensembl"   # ensembl/symbol
+                     ){
+  if(all_gene==TRUE){
+    df <- readxl::read_xlsx(file.path(data_path, file))
+    cat(c(" >- input all gene and run GSEA","\n"))
+  } else {
+    if(list_id=="ensembl"){
+      df <- readxl::read_xlsx(file.path(data_path, file)) %>% 
+        filter(ENSEMBL %in% list)
+    } else if(list_id=="symbol"){
+      df <- readxl::read_xlsx(file.path(data_path, file)) %>% 
+        filter(SYMBOL %in% list)
+    } else{
+      cat(c(" <- error: check list_id","\n"))
+      return(NULL)
+    }
+    cat(c(" >- input selected gene and run ORA"))
+  } 
+  
+  # set organism for human
+  organism <- "org.Hs.eg.db"
+  cat(c(" -> organism type is", organism,"\n"))
+  
+  # $csv file's colume namm of log2 fold change
+  original_gene_list <- df$M
+  # $csv file's colume namm of ENSEMBL ID 
+  names(original_gene_list) <- df$ENSEMBL
+  # omit any NA values and sort the list in decreasing order
+  gsea_gene_list <- na.omit(original_gene_list) %>% 
+    sort(., decreasing = TRUE)
+  cat(c(" -> length of the gene list is",length(gsea_gene_list),"\n"))
+  #run GSEA
+  cat(c(" -> running KEGG", "\n"))
+  gse <- gseGO(geneList = gsea_gene_list, 
+               ont ="ALL", 
+               keyType = "ENSEMBL", 
+               nPerm = 10000, 
+               minGSSize = 3, 
+               maxGSSize = 800, 
+               pvalueCutoff = 0.05, 
+               verbose = TRUE, 
+               OrgDb = organism, 
+               pAdjustMethod = "none")
+  return(gse)
+}
+
+
+# kegg_run ----------------------------------------------------------------
+kegg_run <- function(data_path=data_path,
+                     file,
+                     all_gene=FALSE,
+                     list,
+                     list_id="ensembl"   # ensembl/symbol
+){
+  # select gene in list
+  if(all_gene==TRUE){
+    df <- readxl::read_xlsx(file.path(data_path, file))
+    cat(c(" >- input all gene and run KEGG","\n"))
+  } else {
+    if(list_id=="ensembl"){
+      df <- readxl::read_xlsx(file.path(data_path, file)) %>% 
+        filter(ENSEMBL %in% list)
+    } else if(list_id=="symbol"){
+      df <- readxl::read_xlsx(file.path(data_path, file)) %>% 
+        filter(SYMBOL %in% list)
+    } else{
+      cat(c(" <- error: check list_id","\n"))
+      return(NULL)
+    }
+    cat(c(" >- input selected gene and run ORA"))
+  } 
+  
+  # change KEGG Organism Code from https://www.genome.jp/kegg/catalog/org_list.html 
+  kegg_organism <-  "hsa"  # human is "hsa"
+  cat(c(" -> organism type is", kegg_organism,"\n"))
+  
+  # selcet log2FC value
+  kegg_gene_list <- df$M
+  # Name vector with ENTREZ ids
+  names(kegg_gene_list) <- df$ENTREZID
+  # omit any NA values and sort the list in decreasing order
+  kegg_gene_list<- na.omit(kegg_gene_list) %>% 
+    sort(., decreasing = TRUE)
+  cat(c(" -> length of the gene list is",length(kegg_gene_list),"\n"))
+  # Run KEGG
+  cat(c(" -> running KEGG", "\n"))
+  kegg <- gseKEGG(geneList     = kegg_gene_list,
+                  organism     = kegg_organism,
+                  nPerm        = 10000,
+                  minGSSize    = 3,
+                  maxGSSize    = 800,
+                  pvalueCutoff = 0.05,
+                  pAdjustMethod = "none",
+                  keyType       = "ncbi-geneid")
+  return(kegg)
+}
+
