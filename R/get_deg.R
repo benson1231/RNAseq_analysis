ed4cfc937b767379984f1b32fade7f719c5aad66
benson1231 <144930897+benson1231@users.noreply.github.com>
2024-03-30 16:29:30 +0800

    Update get_deg.R

--- R/get_deg.R
+++ R/get_deg.R
@@ -1,24 +1,20 @@
-### function
-get_deg <- function(data_path=data_path,
-                    file=file,
+get_deg <- function(file=file,
                     log_crit = c(1,-1),
                     dir="all"  # all/up/down
-                    ){
+                    
+){
+  cat(c(" -> load data from",file.path(data_path, file),"\n"))
   data <- readxl::read_xlsx(file.path(data_path, file)) # 讀檔
   if(dir=="all"){
-    data <- data %>% filter(abs(M)>log_crit[1])
+    data <- data %>% filter(M >log_crit[1]| M < log_crit[2])
   }else if(dir=="up"){
     data <- data %>% filter(M > log_crit[1])
   }else if(dir=="down"){
-    data <- data %>% filter(abs(M)< log_crit[2])
+    data <- data %>% filter(M < log_crit[2])
   }else{
     cat(c("<- error, check direction", "\n"))
+    return(NULL)
   }
-  data <- data %>% filter(abs(M)>log_crit)  # filter log2FC criteria
   list <- data$ENSEMBL 
   return(list)# 抓出差異ensembl id
 }
-
-### run function
-BAP_all <- get_deg(data_path = data_path,file = "ip_Y_V_S_BAP_0_deg.xlsx",
-                   log_crit = c(1,-1),dir = "up")
