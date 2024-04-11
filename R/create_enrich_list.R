9010abb0609b17f79d3c135b6c8dc8801e6c0042
benson1231 <144930897+benson1231@users.noreply.github.com>
2024-04-11 19:43:38 +0800

    Create create_enrich_list.R

---
+++ R/create_enrich_list.R
@@ -0,0 +1,29 @@
+create_enrich_list <- function(file_name, type = "ENTREZID") {
+  # 檢查 type 是否有效
+  if (!(type %in% c("ENTREZID", "ENSEMBL"))) {
+    stop("Invalid type. Allowed values are 'ENTREZID' or 'ENSEMBL'.")
+  }
+  # 讀取資料
+  raw_df <- readxl::read_xlsx(file.path(data_path, file_name)) %>% as.data.frame()
+  
+  # 根據不同的類型選擇相應的欄位名稱
+  if (type == "ENTREZID") {
+    id_col <- "ENTREZID"
+  } else {
+    id_col <- "ENSEMBL"
+  }
+  
+  # 處理資料
+  df <- raw_df %>%
+    select(all_of(id_col), M) %>%
+    group_by(across(all_of(id_col))) %>%
+    summarize(across(where(is.numeric), mean)) %>%
+    arrange(desc(M)) 
+  
+  # 創建列表
+  enrich_list <- df$M 
+  names(enrich_list) <- df[[id_col]]
+  enrich_list <-  na.omit(enrich_list)
+  
+  return(enrich_list)
+}
