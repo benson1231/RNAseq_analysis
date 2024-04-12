d305495bd69d4781d2fbd46fc65d5b854e901dd6
benson1231 <144930897+benson1231@users.noreply.github.com>
2024-04-12 19:45:54 +0800

    Update get_deg.R

--- R/get_deg.R
+++ R/get_deg.R
@@ -1,20 +1,38 @@
 get_deg <- function(file=file,
                     log_crit = c(1,-1),
-                    dir="all"  # all/up/down
-                    
-){
+                    dir="all",  # all/up/down
+                    type="ENSEMBL"
+                    ){
+  
+  # 檢查 type 是否有效
+  if (!(type %in% c("ENTREZID", "ENSEMBL"))) {
+    stop("Invalid type. Allowed values are 'ENTREZID' or 'ENSEMBL'.")
+  }
+  
   cat(c(" -> load data from",file.path(data_path, file),"\n"))
   data <- readxl::read_xlsx(file.path(data_path, file)) # 讀檔
+  
+  # filter log2FC criteria
   if(dir=="all"){
     data <- data %>% filter(M >log_crit[1]| M < log_crit[2])
+    cat(c(" -> abs(log2FC) larger than", log_crit[1],"\n"))
   }else if(dir=="up"){
     data <- data %>% filter(M > log_crit[1])
+    cat(c(" -> log2FC larger than", log_crit[1],"\n"))
   }else if(dir=="down"){
     data <- data %>% filter(M < log_crit[2])
+    cat(c(" -> log2FC smaller than", log_crit[2],"\n"))
   }else{
     cat(c("<- error, check direction", "\n"))
     return(NULL)
   }
-  list <- data$ENSEMBL 
-  return(list)# 抓出差異ensembl id
+  
+  # 抓出差異gene id
+  if(type=="ENTREZID"){
+    list <- data$ENTREZID %>% na.omit()
+  }else{
+    list <- data$ENSEMBL
+  }
+  
+  return(list)
 }
