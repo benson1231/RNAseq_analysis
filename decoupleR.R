d95df4a281df64acda959c1f30605357a5dba534
benson1231 <144930897+benson1231@users.noreply.github.com>
2024-05-10 18:19:29 +0800

    Update decoupleR.R

--- decoupleR.R
+++ decoupleR.R
@@ -7,10 +7,28 @@ library(ggplot2)
 library(pheatmap)
 library(ggrepel)
 
-# https://www.bioconductor.org/packages/release/bioc/vignettes/decoupleR/inst/doc/pw_bk.html
+source("RNAseq_function.R")
 
-inputs_dir <- system.file("extdata", package = "decoupleR")
-data <- readRDS(file.path(inputs_dir, "bk_data.rds"))
+# count data input --------------------------------------------------------
+data_path <- "/Users/benson/Documents/raw_data/RNA-seq1-3/Vitro"
+group_names <- c("ip_L_V_L_CON", "ip_L_V_L_DMS", "ip_L_V_L_AZA", "ip_L_V_L_DAC", "ip_L_V_L_AS",
+                 "ip_L_V_L_CO", "ip_L_V_L_LCD", "ip_L_V_L_HCD", "ip_L_V_L_BAP", "ip_L_V_L_AS_BAP",
+                 "ip_L_V_L_CO_BAP", "ip_L_V_L_LCD_BAP", "ip_L_V_L_HCD_BAP", "ip_Y_V_S_CON",
+                 "ip_Y_V_S_DMS", "ip_Y_V_S_AZA", "ip_Y_V_S_DAC", "ip_Y_V_S_AS", "ip_Y_V_S_CO",
+                 "ip_Y_V_S_LCD", "ip_Y_V_S_HCD", "ip_Y_V_S_BAP", "ip_Y_V_S_AS_BAP", "ip_Y_V_S_CO_BAP",
+                 "ip_Y_V_S_LCD_BAP", "ip_Y_V_S_HCD_BAP")
+
+mycount_77 <- "/Users/benson/Documents/project/RNA-seq1-3/myTMM.RDS" %>% 
+  readRDS() %>% as.data.frame()
+mycount_df <- mycount_77 %>% 
+  select(.,all_of(group_names))
+gene_df <- "/Users/benson/Documents/project/RNA-seq1-3/anno_gene.RDS" %>% 
+  readRDS() %>%
+  select(ENSEMBL,SYMBOL,ENTREZID)
+anno_df <- "/Users/benson/Documents/project/RNA-seq1-3/anno_gene.RDS" %>% 
+  readRDS()
+
+# https://www.bioconductor.org/packages/release/bioc/vignettes/decoupleR/inst/doc/pw_bk.html
 
 # input -------------------------------------------------------------------
 count <- mycount_df %>% rownames_to_column("ENSEMBL") %>% left_join(gene_df,"ENSEMBL") %>% 
@@ -21,8 +39,9 @@ count <- mycount_df %>% rownames_to_column("ENSEMBL") %>% left_join(gene_df,"ENS
 design <- colnames(mycount_df) %>% as.data.frame() %>% setNames("design")
 design
 
-
-net <- get_progeny(organism = 'human', top = 500)
+# net <- get_progeny(organism = 'human', top = 500)
+# saveRDS(net,"net.RDS")
+net <- "/Users/benson/Documents/project/RNA-seq1-3/net.RDS" %>% readRDS()
 net
 sample_acts <- run_mlm(mat=count, net=net, .source='source', .target='target',
                        .mor='weight', minsize = 5)
@@ -39,7 +58,7 @@ sample_acts_mat <- scale(sample_acts_mat)
 
 # Choose color palette
 palette_length = 100
-my_color = colorRampPalette(c("Darkblue", "white","red"))(palette_length)
+my_color = colorRampPalette(c("darkblue", "white","red"))(palette_length)
 
 my_breaks <- c(seq(-3, 0, length.out=ceiling(palette_length/2) + 1),
                seq(0.05, 3, length.out=floor(palette_length/2)))
@@ -48,9 +67,9 @@ my_breaks <- c(seq(-3, 0, length.out=ceiling(palette_length/2) + 1),
 pheatmap(sample_acts_mat, border_color = NA, color=my_color, breaks = my_breaks) 
 
 
-# new ---------------------------------------------------------------------
+# pathway ---------------------------------------------------------------------
 # example
-deg <- get_df("ip_Y_V_S_CO_0_deg.xlsx",de=T) %>% group_by(gene) %>%
+deg <- get_df("ip_Y_V_S_HCD_0_deg.xlsx",de=T) %>% group_by(gene) %>%
   summarize(across(where(is.numeric), sum)) %>% na.omit() %>% 
   column_to_rownames(., var = "gene")
 
@@ -68,9 +87,9 @@ ggplot(contrast_acts, aes(x = reorder(source, score), y = score)) +
         axis.text.y = element_text(size =10, face= "bold"),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) +
-  xlab("Pathways")
+  xlab("Pathways") + ggtitle("Y-ST-Co_BAP")
 
-pathway <- 'p53'
+pathway <- "Androgen"
 
 df <- net %>%
   filter(source == pathway) %>%
@@ -96,10 +115,9 @@ ggplot(df, aes(x = weight, y = logFC, color = color)) + geom_point() +
   ggtitle(pathway)
 
 # TF ----------------------------------------------------------------------
-de <- get_df("ip_Y_V_S_CO_0_deg.xlsx",all = T) %>% group_by(SYMBOL) %>%
-  summarize(across(where(is.numeric), sum)) %>% na.omit() %>% 
-  column_to_rownames(., var = "SYMBOL") %>% .[,6:7]
-net_TF <- get_collectri(organism='human', split_complexes=FALSE)
+# net_TF <- get_collectri(organism='human', split_complexes=FALSE)
+# saveRDS(net_TF,"net_TF.RDS")
+net_TF <- "/Users/benson/Documents/project/RNA-seq1-3/net_TF.RDS" %>% readRDS()
 net_TF
 sample_TF <- run_ulm(mat=count, net=net_TF, .source='source', .target='target',
                        .mor='mor', minsize = 5)
@@ -137,8 +155,15 @@ my_breaks <- c(seq(-3, 0, length.out=ceiling(palette_length/2) + 1),
 pheatmap(sample_TF_mat, border_color = NA, color=my_color, breaks = my_breaks) 
 
 ### logFC
+file_name <- "ip_Y_V_S_HCD_0_deg.xlsx"
+de_FC <- get_df(file_name ,de=T) %>% group_by(gene) %>%
+  summarize(across(where(is.numeric), sum)) %>% na.omit() %>% 
+  column_to_rownames(., var = "gene")
+de <- get_df(file_name ,all = T) %>% group_by(SYMBOL) %>%
+  summarize(across(where(is.numeric), sum)) %>% na.omit() %>% 
+  column_to_rownames(., var = "SYMBOL") %>% .[,6:7]
 # Run ulm
-contrast_TF <- run_ulm(mat=deg[, "logFC", drop=FALSE], net=net_TF, .source='source', .target='target',
+contrast_TF <- run_ulm(mat=de_FC[, "logFC", drop=FALSE], net=net_TF, .source='source', .target='target',
                          .mor='mor', minsize = 5)
 contrast_TF
 
@@ -167,10 +192,10 @@ ggplot(f_contrast_TF, aes(x = reorder(source, score), y = score)) +
         axis.text.y = element_text(size =10, face= "bold"),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) +
-  xlab("TFs")
+  xlab("TFs") + ggtitle("Y-ST-HCd")
 
 # 
-tf <- 'HIF1A'
+tf <- 'MTF1'
 
 df <- net_TF %>%
   filter(source == tf) %>%
