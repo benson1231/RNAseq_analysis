4ee78bed7155b364fd00cdcb25cbcfdc21673058
benson1231 <144930897+benson1231@users.noreply.github.com>
2024-05-13 15:02:53 +0800

    Update decoupleR.R

--- decoupleR.R
+++ decoupleR.R
@@ -1,44 +1,43 @@
-## We load the required packages
+### We load the required packages -------------------------------------------
 library(decoupleR)
 library(OmnipathR)
-library(tibble)
-library(tidyr)
-library(ggplot2)
+library(tidyverse)
 library(pheatmap)
 library(ggrepel)
 
 source("RNAseq_function.R")
 
-# count data input --------------------------------------------------------
+### count data input --------------------------------------------------------
 data_path <- "/Users/benson/Documents/raw_data/RNA-seq1-3/Vitro"
+# annotation df
+gene_df <- "/Users/benson/Documents/project/RNA-seq1-3/anno_gene.RDS" %>% 
+  readRDS() %>% select(ENSEMBL,SYMBOL,ENTREZID)
+anno_df <- "/Users/benson/Documents/project/RNA-seq1-3/anno_gene.RDS" %>% 
+  readRDS()
+# groups
 group_names <- c("ip_L_V_L_CON", "ip_L_V_L_DMS", "ip_L_V_L_AZA", "ip_L_V_L_DAC", "ip_L_V_L_AS",
                  "ip_L_V_L_CO", "ip_L_V_L_LCD", "ip_L_V_L_HCD", "ip_L_V_L_BAP", "ip_L_V_L_AS_BAP",
                  "ip_L_V_L_CO_BAP", "ip_L_V_L_LCD_BAP", "ip_L_V_L_HCD_BAP", "ip_Y_V_S_CON",
                  "ip_Y_V_S_DMS", "ip_Y_V_S_AZA", "ip_Y_V_S_DAC", "ip_Y_V_S_AS", "ip_Y_V_S_CO",
                  "ip_Y_V_S_LCD", "ip_Y_V_S_HCD", "ip_Y_V_S_BAP", "ip_Y_V_S_AS_BAP", "ip_Y_V_S_CO_BAP",
                  "ip_Y_V_S_LCD_BAP", "ip_Y_V_S_HCD_BAP")
-
+# count df
 mycount_77 <- "/Users/benson/Documents/project/RNA-seq1-3/myTMM.RDS" %>% 
   readRDS() %>% as.data.frame()
 mycount_df <- mycount_77 %>% 
   select(.,all_of(group_names))
-gene_df <- "/Users/benson/Documents/project/RNA-seq1-3/anno_gene.RDS" %>% 
-  readRDS() %>%
-  select(ENSEMBL,SYMBOL,ENTREZID)
-anno_df <- "/Users/benson/Documents/project/RNA-seq1-3/anno_gene.RDS" %>% 
-  readRDS()
-
-# https://www.bioconductor.org/packages/release/bioc/vignettes/decoupleR/inst/doc/pw_bk.html
-
-# input -------------------------------------------------------------------
-count <- mycount_df %>% rownames_to_column("ENSEMBL") %>% left_join(gene_df,"ENSEMBL") %>% 
+count <- mycount_df %>% 
+  rownames_to_column("ENSEMBL") %>% 
+  left_join(gene_df,"ENSEMBL") %>% 
   group_by(SYMBOL) %>%
   summarize(across(where(is.numeric), sum)) %>% na.omit() %>% 
   column_to_rownames(., var = "SYMBOL")
-
+# design
 design <- colnames(mycount_df) %>% as.data.frame() %>% setNames("design")
 design
 
+### pathway heatmap ---------------------------------------------------------
+### load pathway data
 # net <- get_progeny(organism = 'human', top = 500)
 # saveRDS(net,"net.RDS")
 net <- "/Users/benson/Documents/project/RNA-seq1-3/net.RDS" %>% readRDS()
@@ -63,19 +62,34 @@ my_color = colorRampPalette(c("darkblue", "white","red"))(palette_length)
 my_breaks <- c(seq(-3, 0, length.out=ceiling(palette_length/2) + 1),
                seq(0.05, 3, length.out=floor(palette_length/2)))
 
-# Plot
+### heatmap
 pheatmap(sample_acts_mat, border_color = NA, color=my_color, breaks = my_breaks) 
 
 
-# pathway ---------------------------------------------------------------------
-# example
-deg <- get_df("ip_Y_V_S_HCD_0_deg.xlsx",de=T) %>% group_by(gene) %>%
+
+### two groups comparing pathway --------------------------------------------
+### read file
+file_name <- "ip_Y_V_S_HCD_BAP_0_deg.xlsx"   # change
+deg_FC <- get_df(file_name ,de=T,log_crit = c(1,-1)) %>% 
+  group_by(gene) %>%
   summarize(across(where(is.numeric), sum)) %>% na.omit() %>% 
   column_to_rownames(., var = "gene")
+deg_df <- get_df(file_name ,all = T) %>% group_by(SYMBOL) %>%
+  summarize(across(where(is.numeric), sum)) %>% na.omit() %>% 
+  column_to_rownames(., var = "SYMBOL") %>% 
+  rownames_to_column("ID") %>% 
+  .[,c(1,5,6,7,8)] %>% 
+  setNames(c("ID","treat","control","M","D"))%>% 
+  mutate(Average_expression =(treat+control)/2) %>% 
+  .[,c(1,4,6)] %>% 
+  setNames(c("ID","logFC","Average_expression"))
 
-contrast_acts <- run_mlm(mat=deg, net=net, .source='source', .target='target',
+### run MLM analysis
+contrast_acts <- run_mlm(mat=deg_FC, net=net, .source='source', .target='target',
                          .mor='weight', minsize = 5)
 contrast_acts
+
+### pathway enrich plot
 ggplot(contrast_acts, aes(x = reorder(source, score), y = score)) + 
   geom_bar(aes(fill = score), stat = "identity") +
   scale_fill_gradient2(low = "darkblue", high = "indianred", 
@@ -87,26 +101,25 @@ ggplot(contrast_acts, aes(x = reorder(source, score), y = score)) +
         axis.text.y = element_text(size =10, face= "bold"),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) +
-  xlab("Pathways") + ggtitle("Y-ST-Co_BAP")
+  xlab("Pathways") + ggtitle("Y-ST-Co_BAP")   # change title
 
-pathway <- "Androgen"
+### Specific pathway related genes
+pathway <- "EGFR"   # change
+logFC_criteria <- 1   # change
 
-df <- net %>%
+### filter pathway specific genes
+path <- net %>%
   filter(source == pathway) %>%
   arrange(target) %>%
   mutate(ID = target, color = "3") %>%
-  column_to_rownames('target')
-inter <- sort(intersect(rownames(deg),rownames(df)))
-df <- df[inter, ]
-df[c('logFC')] <- deg[inter, ]
-df <- df %>%
-  mutate(color = if_else(weight > 0 & logFC > 0, '1', color)) %>%
-  mutate(color = if_else(weight > 0 & logFC < 0, '2', color)) %>%
-  mutate(color = if_else(weight < 0 & logFC > 0, '2', color)) %>%
-  mutate(color = if_else(weight < 0 & logFC < 0, '1', color))
-
-ggplot(df, aes(x = weight, y = logFC, color = color)) + geom_point() +
-  scale_colour_manual(values = c("red","royalblue3","grey")) +
+  column_to_rownames('target') %>% 
+  left_join(deg_df,"ID") %>% 
+  mutate(color = if_else(logFC > logFC_criteria, '1', color)) %>%
+  mutate(color = if_else(logFC < logFC_criteria & logFC > negative(logFC_criteria), '2', color)) %>%   
+  mutate(color = if_else(logFC < negative(logFC_criteria), '3', color)) 
+### MD plot
+ggplot(path, aes(x = log(Average_expression), y = logFC, color = color)) + geom_point() +
+  scale_colour_manual(values = c("red","grey","royalblue3")) +
   geom_label_repel(aes(label = ID)) + 
   theme_minimal() +
   theme(legend.position = "none") +
@@ -114,7 +127,9 @@ ggplot(df, aes(x = weight, y = logFC, color = color)) + geom_point() +
   geom_hline(yintercept = 0, linetype = 'dotted') +
   ggtitle(pathway)
 
-# TF ----------------------------------------------------------------------
+
+### TFs heatmap -------------------------------------------------------------
+### load pathway data
 # net_TF <- get_collectri(organism='human', split_complexes=FALSE)
 # saveRDS(net_TF,"net_TF.RDS")
 net_TF <- "/Users/benson/Documents/project/RNA-seq1-3/net_TF.RDS" %>% readRDS()
@@ -123,7 +138,7 @@ sample_TF <- run_ulm(mat=count, net=net_TF, .source='source', .target='target',
                        .mor='mor', minsize = 5)
 sample_TF
 
-### plot
+# plot
 n_tfs <- 25
 # Transform to wide matrix
 sample_TF_mat <- sample_TF %>%
@@ -151,18 +166,24 @@ my_color = colorRampPalette(c("Darkblue", "white","red"))(palette_length)
 my_breaks <- c(seq(-3, 0, length.out=ceiling(palette_length/2) + 1),
                seq(0.05, 3, length.out=floor(palette_length/2)))
 
-# Plot
+### heatmap
 pheatmap(sample_TF_mat, border_color = NA, color=my_color, breaks = my_breaks) 
 
-### logFC
+
+### two groups comparing TFs ------------------------------------------------
 file_name <- "ip_Y_V_S_HCD_0_deg.xlsx"
 de_FC <- get_df(file_name ,de=T) %>% group_by(gene) %>%
   summarize(across(where(is.numeric), sum)) %>% na.omit() %>% 
   column_to_rownames(., var = "gene")
 de <- get_df(file_name ,all = T) %>% group_by(SYMBOL) %>%
   summarize(across(where(is.numeric), sum)) %>% na.omit() %>% 
-  column_to_rownames(., var = "SYMBOL") %>% .[,6:7]
-# Run ulm
+  column_to_rownames(., var = "SYMBOL") %>% 
+  rownames_to_column("ID") %>% 
+  .[,c(1,5,6,7,8)] %>% 
+  setNames(c("ID","treat","control","M","D"))%>% 
+  mutate(Average_expression =(treat+control)/2) %>% 
+  .[,c(1,4,6)] %>% setNames(c("ID","logFC","Average_expression"))
+### Run ULM
 contrast_TF <- run_ulm(mat=de_FC[, "logFC", drop=FALSE], net=net_TF, .source='source', .target='target',
                          .mor='mor', minsize = 5)
 contrast_TF
@@ -180,7 +201,7 @@ tfs <- f_contrast_TF %>%
 f_contrast_TF <- f_contrast_TF %>%
   filter(source %in% tfs)
 
-# Plot
+### TF enrich plot
 ggplot(f_contrast_TF, aes(x = reorder(source, score), y = score)) + 
   geom_bar(aes(fill = score), stat = "identity") +
   scale_fill_gradient2(low = "darkblue", high = "indianred", 
@@ -192,30 +213,27 @@ ggplot(f_contrast_TF, aes(x = reorder(source, score), y = score)) +
         axis.text.y = element_text(size =10, face= "bold"),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) +
-  xlab("TFs") + ggtitle("Y-ST-HCd")
+  xlab("TFs") + ggtitle("")   # change
 
-# 
-tf <- 'MTF1'
+### Specific TF related genes -----------------------------------------------
+tf <- 'JUN'   # change
+logFC_criteria <- 1    # change
 
+# filter TF related genes
 df <- net_TF %>%
   filter(source == tf) %>%
   arrange(target) %>%
   mutate(ID = target, color = "3") %>%
-  column_to_rownames('target')
+  column_to_rownames('target') %>% 
+  left_join(de,"ID") %>% 
+  mutate(color = if_else(logFC > logFC_criteria, '1', color)) %>%
+  mutate(color = if_else(logFC < logFC_criteria & logFC > negative(logFC_criteria), '2', color)) %>%   
+  mutate(color = if_else(logFC < negative(logFC_criteria), '3', color)) 
 
-inter <- sort(intersect(rownames(de),rownames(df)))
-df <- df[inter, ]
-df[,'logfc'] <- de[inter, ]$M
-df[,'D'] <- de[inter, ]$D
-df <- df %>%
-  mutate(color = if_else(mor > 0 & logfc > 0, '1', color)) %>%
-  mutate(color = if_else(mor > 0 & logfc < 0, '2', color)) %>%
-  mutate(color = if_else(mor < 0 & logfc > 0, '2', color)) %>%
-  mutate(color = if_else(mor < 0 & logfc < 0, '1', color))
-
-ggplot(df, aes(x = logfc, y = log(D), color = color, size=abs(mor))) +
+### MD plot
+ggplot(df, aes(x = log(Average_expression), y = logFC, color = color, size=abs(mor))) +
   geom_point() +
-  scale_colour_manual(values = c("red","royalblue3","grey")) +
+  scale_colour_manual(values = c("red","grey","royalblue3")) +
   geom_label_repel(aes(label = ID, size=1)) + 
   theme_minimal() +
   theme(legend.position = "none") +
